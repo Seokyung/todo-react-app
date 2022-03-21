a78860a617c2dab8a102c7793347c2407f4d1c95
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-21 22:38:06 +0900

    Todo List
    배열에 담긴 Todo 아이템들을 렌더링해주고 material-ui를 이용해 디자인을 추가해줌

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import Todo from './Todo.js';
+import { Paper, List } from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
   //App 컴포넌트 생성자 코드 - 생성자에서 props를 넘겨받고 this.state에서 item을 초기화해줌
   constructor(props) {
     super(props);
+    //1. item -> items 배열로
     this.state = {
-      item: { id: 0, title: "Hello World 1", done: true }, //this.state.item을 이용해 item 오브젝트에 접근
-      item2: { id: 1, title: "Hello World 2", done: false},
+      items: [
+        { id: 0, title: "Hello World 1", done: true }, //this.state.item을 이용해 item 오브젝트에 접근
+        { id: 1, title: "Hello World 2", done: false},
+      ],
     };
   }
 
   render() {
+    //2. 자바스크립트가 제공하는 map 함수를 이용해 배열을 반복해 <Todo... /> 컴포넌트 생성
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} /> // item={변수}를 아용해 props로 매개변수를 넘김
+          ))}
+        </List>
+      </Paper>
+    );
+
+    //3. 생성된 컴포넌트 리턴    
     return (
-      <div className="App">        
-        <Todo item={this.state.item} /> {/* item={변수}를 아용해 props로 매개변수를 넘김 */}
-        <Todo item={this.state.item2} />
+      <div className="App">
+        {todoItems}
       </div>
     )
   }
