ebcdaaf6d43e619a0977715094faee7f1d036aee
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-22 01:17:21 +0900

    Todo 추가
    Todo 추가를 위한 UI와 백엔드 콜을 대신할 가짜 함수를 작성하고 Todo 추가를 통해 이벤트 핸들러 함수를 구현하는 방법과 핸들러 함수를 UI에 연결하는 방법을 습득함

---
+++ src/AddTodo.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { TextField, Paper, Button, Grid } from "@material-ui/core";
+
+class AddTodo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { item: { title: "" } }; //사용자의 입력을 저장할 오브젝트
+        this.add = props.add //props의 함수를 this.add에 연결
+    }
+
+    //onInputChange 함수 작성 - 사용자가 input 필드에 키를 하나 입력할 때마다 문자열을 자바스크립트 오브젝트에 저장함
+    onInputChange = (e) => { //Event e를 매개변수로 받음 - event: 어떤 일이 일어났을 때의 상태와 그 일에 대한 정보를 담고 있음
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value; //e.target.value에 현재 화면의 InputField에 입력된 글자들이 담겨있음
+        this.setState({ item: thisItem }); //setState를 통해 item을 업데이트해 사용자의 Todo 아이템을 임시로 저장함
+        console.log(thisItem);
+    }
+
+    //onButtonClick 함수 작성 - App 컴포넌트에서 add 함수를 props로 넘겨받아 사용
+    onButtonClick = () => {
+        this.add(this.state.item); //add 함수 사용
+        this.setSState({ item: { title: "" } });
+    }
+
+    //enterKeyEventHandler 함수 작성 - 키보드의 키 이벤트 발생 시 항상 실행(엔터키)
+    enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter') {
+            this.onButtonClick(); //+버튼 클릭과 동일한 기능을 하므로 +버튼 클릭 함수 재사용함
+        }
+    }
+
+    render() {
+        //onInputChange, onButtonClick 함수 연결
+        return (
+            <Paper style={{ margin: 16, padding: 16 }}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                        <TextField
+                            placeholder="Add Todo Here"
+                            fullWidth
+                            onChange={this.onInputChange} //이벤트 핸들러 함수: 어떤 이벤트가 발생할 때마다 onChange() 실행 -> 이벤트 e가 매개변수로 넘어옴
+                            value={this.state.item.title}
+                            onKeyPress={this.enterKeyEventHandler} //onKeyPress() : 키보드의 키가 눌릴 때마다 실행되는 이벤트 핸들러
+                        />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button
+                            fullWidth
+                            color="secondary"
+                            variant="outlined"
+                            onClick={this.onButtonClick}
+                        >
+                            +
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        );
+    }
+}
+
+export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Todo from './Todo.js';
-import { Paper, List } from "@material-ui/core";
+import AddTodo from './AddTodo.js';
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
   //App 컴포넌트 생성자 코드 - 생성자에서 props를 넘겨받고 this.state에서 item을 초기화해줌
   constructor(props) {
     super(props);
-    //1. item -> items 배열로
+    //item -> items 배열로
     this.state = {
       items: [
         { id: 0, title: "Hello World 1", done: true }, //this.state.item을 이용해 item 오브젝트에 접근
@@ -16,8 +17,18 @@ class App extends React.Component {
     };
   }
 
+  //add 함수 추가
+  add = (item) => {
+    const thisItems = this.state.itmes;
+    item.id = "ID-" + thisItems.length; //key를 위한 id 추가
+    item.done = false; //done 초기화
+    thisItems.push(item) //리스트에 아이템 추가
+    this.setState({ items: thisItems }); //업데이트는 반드시 this,setState로 해야함
+    console.log("items : ", this.state.items);
+  }
+
   render() {
-    //2. 자바스크립트가 제공하는 map 함수를 이용해 배열을 반복해 <Todo... /> 컴포넌트 생성
+    //자바스크립트가 제공하는 map 함수를 이용해 배열을 반복해 <Todo... /> 컴포넌트 생성
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
@@ -28,10 +39,13 @@ class App extends React.Component {
       </Paper>
     );
 
-    //3. 생성된 컴포넌트 리턴    
+    //생성된 컴포넌트 리턴, add 함수 연결
     return (
       <div className="App">
-        {todoItems}
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
       </div>
     )
   }
