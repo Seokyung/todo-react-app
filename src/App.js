37905292f344577640020d723f996be1e033890a
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-21 19:36:58 +0900

    props and state
    Todo 컴포넌트의 오브젝트를 가져오고 props와 state를 통해 매개변수 값을 넘겨줌

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //App 컴포넌트 생성자 코드 - 생성자에서 props를 넘겨받고 this.state에서 item을 초기화해줌
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: { id: 0, title: "Hello World 1", done: true }, //this.state.item을 이용해 item 오브젝트에 접근
+      item2: { id: 1, title: "Hello World 2", done: false},
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">        
+        <Todo item={this.state.item} /> {/* item={변수}를 아용해 props로 매개변수를 넘김 */}
+        <Todo item={this.state.item2} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class Todo extends React.Component {
+    //Todo 컴포넌트 생성자 코드
+    constructor(props) {
+        super(props); //super를 이용해 props 오브젝트 초기화
+        this.state = { item: props.item }; //this.state를 item변수와 props.item으로 초기화
+    }
+
     render() {
         return (
             <div classname="Todo">
-                <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
-                <label for="todo0">Todo 컴포넌트 만들기</label>
+                <input type="checkbox" id={this.state.item.id} name={this.state.item.id} checked={this.state.item.done}/>
+                <label id={this.state.item.id}>{this.state.item.title}</label>
             </div>
         );
     }
