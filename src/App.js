e410b841f6cc1a3a23fcc31ccbde4ef7e0afb633
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-24 02:36:04 +0900

    Todo 추가 오류 해결
    items 배열 초기값이 없어서 this.state.items.length>0로 조건을 주면 오류가 났다.. 조건을 0이 아니면(!==0)으로 바꿔줬더니 고쳐졌다.

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ class App extends React.Component {
 
   //add 함수 추가 (Todo 추가하는 함수)
   add = (item) => {
-    const thisItems = this.state.itmes;
+    const thisItems = this.state.items;
     item.id = "ID-" + thisItems.length; //key를 위한 id 추가
+    console.log("id : ", thisItems.length)
     item.done = false; //done 초기화
     thisItems.push(item) //리스트에 아이템 추가
     this.setState({ items: thisItems }); //업데이트는 반드시 this,setState로 해야함
@@ -38,7 +39,7 @@ class App extends React.Component {
 
   render() {
     //자바스크립트가 제공하는 map 함수를 이용해 배열을 반복해 <Todo... /> 컴포넌트 생성
-    var todoItems = this.state.items.length > 0 && (
+    var todoItems = (this.state.items.length !== 0) && ( //배열 초기값이 없어서 this.state.items.length>0로 조건을 주면 오류가 났다.. 조건을 0이 아니면으로 바꿔줬더니 고쳐졌다
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
--- src/Todo.js
+++ src/Todo.js
@@ -13,13 +13,21 @@ class Todo extends React.Component {
     //Todo 컴포넌트 생성자 코드
     constructor(props) {
         super(props); //super를 이용해 props 오브젝트 초기화
-        this.state = { item: props.item }; //this.state를 item변수와 props.item으로 초기화
+        this.state = { item: props.item, readOnly: true }; //this.state를 item변수와 props.item으로 초기화, readOnly 상태변수 추가
         this.delete = props.delete;
     }
 
     //deleteEventHandler 함수
     deleteEventHandler = () => {
         this.delete(this.state.item) //this.state.item을 이용해 item 오브젝트에 접근
+    }
+
+    //title 클릭 시 읽기모드가 해제되는 함수
+    offReadOnlyMode = () => {
+        console.log("Event!", this.state.readOnly)
+        this.setState({ readOnly: false }, () => { //readOnly를 false로 바꿔줌
+            console.log("ReadOnly? ", this.state.readOnly)
+        });
     }
 
     render() {
@@ -30,7 +38,11 @@ class Todo extends React.Component {
 
                 <ListItemText>
                     <InputBase
-                        inputProps={{ "aria-label": "naked" }}
+                        inputProps={{
+                            "aria-label": "naked",
+                            readOnly: this.state.readOnly, //readOnly 상태 연결
+                        }}
+                        onClick={this.offReadOnlyMode} //함수 연결
                         type="text"
                         id={item.id} //각 리스트를 구분하려고 id(고유번호)를 연결
                         name={item.id} // 각 리스트를 구분하려고 id를 연결
