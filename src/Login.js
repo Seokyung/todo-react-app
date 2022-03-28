56edec8d88ddc0b632feaafd5142bf61685499e2
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-29 03:00:39 +0900

    AppRouter 추가
    react-router-dom 라이브러리 설치, AppRouter 컴포넌트를 만들어 url이 요청될 때 맨 처음 렌더링되는 컴포넌트를 App에서 AppRouter로 바꿔줌

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+//로그인 컴포넌트
+import React from "react";
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>로그인 페이지</h1>;
+    }
+}
+
+export default Login;
