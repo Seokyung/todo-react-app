79201d34c8b77edbca0e8a8701984329de7f1439
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-30 21:21:12 +0900

    ApiService catch & redirect error 임시조치
    **수정 필요! (원래 조건은 error.status === 403뿐이지만 error.status를 자꾸 못받아와서 임시조치해줬다.)

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,26 +2,38 @@ import { API_BASE_URL } from "../app-config";
 
 //백엔드로 요청 보낼 때 사용할 유틸리티 함수
 export function call(api, method, request) {
-    let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
-        url: API_BASE_URL + api,
-        method: method,
-    };
+  let options = {
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+    url: API_BASE_URL + api,
+    method: method,
+  };
+  
+  if (request) {
+    // GET method
+    options.body = JSON.stringify(request);
+  }
 
-    if (request) {
-        //GET method
-        options.body = JSON.stringify(request);
-    }
+  //console.log("url : ",options.url);
+  //console.log("request : ",request);
 
-    return fetch(options.url, options).then((response) =>
-        response.json().then((json) => {
-            if(!response.ok) {
-                //response.ok가 true이면 정상적인 응답을 받은 것이고, 아니면 에러 응답을 받은 것임
-                return Promise.reject(json);
-            }
-            return json;
-        })
-    );
+  return fetch(options.url, options)
+    .then((response) =>
+      response.json().then((json) => {
+        if (!response.ok) {
+          //response.ok가 true이면 정상적인 응답을 받은 것이고, 아니면 에러 응답을 받은 것임
+          return Promise.reject(json);          
+        }
+        return json;
+      })
+    )
+    .catch((error) => {
+      // 추가된 부분
+      //console.log("error.status : ", error.status);
+      if (error.status === undefined || error.status === 403) { //**수정 필요! (원래 조건은 error.status === 403뿐이지만 error.status를 자꾸 못받아와서 임시조치해줬다.)
+        window.location.href = "/login"; // redirect
+      }
+      return Promise.reject(error);
+    });
 }
